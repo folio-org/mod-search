f86e5fca0f2e670fbbdedfb37fcfe5e5ce382b73
Pavel Filippov <pavel_filippov@epam.com>
2021-01-15 18:07:15 +0300

    MSEARCH-37 Remove unused import in TenantController

--- src/main/java/org/folio/search/controller/TenantController.java
+++ src/main/java/org/folio/search/controller/TenantController.java
@@ -1,7 +1,6 @@
 package org.folio.search.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.folio.tenant.rest.resource.TenantApi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

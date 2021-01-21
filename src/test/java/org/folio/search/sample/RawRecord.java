bc242872a56c416e0d9bf8586c3d2e6c353c525a
Bohdan Suprun <Bohdan_Suprun@epam.com>
2021-01-21 08:27:08 +0200

    MSEARCH-25: Add sample instance.

---
+++ src/test/java/org/folio/search/sample/RawRecord.java
@@ -0,0 +1,22 @@
+package org.folio.search.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class RawRecord {
+  private String id;
+  @Getter(onMethod_ = @__(@JsonAnyGetter))
+  private Map<String, Object> unknown = new HashMap<>();
+
+  @JsonAnySetter
+  public void set(String name, Object value) {
+    unknown.put(name, value);
+  }
+}
---
+++ src/test/java/org/folio/search/sample/SampleInstances.java
@@ -0,0 +1,26 @@
+package org.folio.search.sample;
+
+import static org.folio.dbschema.ObjectMapperTool.getMapper;
+
+import java.io.InputStream;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SampleInstances {
+  private static final RawRecord SEMANTIC_WEB = readSampleInstance("semantic_web_primer.json");
+
+  public static RawRecord getSemanticWeb() {
+    return SEMANTIC_WEB;
+  }
+
+  @SneakyThrows
+  private static RawRecord readSampleInstance(String fileName) {
+    return getMapper().readValue(getSampleAsStream(fileName), RawRecord.class);
+  }
+
+  private static InputStream getSampleAsStream(String fileName) {
+    return SampleInstances.class.getResourceAsStream("/samples/" + fileName);
+  }
+}

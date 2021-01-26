3ded3ce5a05264fe33ef905a38bd842482599ebb
Bohdan Suprun <Bohdan_Suprun@epam.com>
2021-01-26 18:28:53 +0200

    MSEARCH-9: Rename ConfigControllerIT class to match pattern.

--- src/test/java/org/folio/search/controller/ConfigControllerIT.java
+++ src/test/java/org/folio/search/controller/ConfigControllerIT.java
@@ -25,7 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @IntegrationTest
-class ConfigControllerTest extends BaseIntegrationTest {
+class ConfigControllerIT extends BaseIntegrationTest {
   @Autowired
   private KafkaTemplate<String, Object> kafkaTemplate;
 

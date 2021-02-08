4bac4de5b1a830c608d19c84dc0e80f2b812bc3a
Bohdan Suprun <Bohdan_Suprun@epam.com>
2021-02-08 10:17:18 +0200

    MSEARCH-40: Add unit tests for field setters.

---
+++ src/test/java/org/folio/search/service/setter/instance/SortContributorsSetterTest.java
@@ -0,0 +1,64 @@
+package org.folio.search.service.setter.instance;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.folio.search.utils.TestUtils.OBJECT_MAPPER;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.folio.search.domain.dto.InstanceContributors;
+import org.folio.search.utils.JsonConverter;
+import org.folio.search.utils.types.UnitTest;
+import org.junit.jupiter.api.Test;
+
+@UnitTest
+class SortContributorsSetterTest {
+  private static final String CONTRIBUTORS = "contributors";
+
+  private final JsonConverter converter = new JsonConverter(OBJECT_MAPPER);
+  private final SortContributorsSetter contributorsSetter =
+    new SortContributorsSetter(converter);
+
+  @Test
+  void shouldReturnFirstContributorIfNoPrimary() {
+    var map = contributorsToMap(new InstanceContributors().name("first"),
+      new InstanceContributors().name("second"));
+
+    assertThat(contributorsSetter.getFieldValue(map)).isEqualTo("first");
+  }
+
+  @Test
+  void shouldReturnPrimaryContributorRegardlessPosition() {
+    var map = contributorsToMap(new InstanceContributors().name("first"),
+      new InstanceContributors().name("second").primary(true));
+
+    assertThat(contributorsSetter.getFieldValue(map)).isEqualTo("second");
+  }
+
+  @Test
+  void shouldReturnNullIfEmptyMap() {
+    assertNull(contributorsSetter.getFieldValue(null));
+  }
+
+  @Test
+  void shouldReturnNullIfNoContributors() {
+    assertNull(contributorsSetter.getFieldValue(Map.of("title", "title")));
+  }
+
+  @Test
+  void shouldReturnNullIfContributorsIsEmpty() {
+    assertNull(contributorsSetter.getFieldValue(Map.of(CONTRIBUTORS, emptyList())));
+  }
+
+  private Map<String, Object> contributorsToMap(InstanceContributors... contributors) {
+    List<Object> contributorsArray = new ArrayList<>();
+
+    for (InstanceContributors contributor : contributors) {
+      contributorsArray.add(converter.convert(contributor, Map.class));
+    }
+
+    return Map.of(CONTRIBUTORS, contributorsArray);
+  }
+}

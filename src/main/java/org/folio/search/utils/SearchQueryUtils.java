9478e3ec6c7ce77a6f79c762a1c39cf59e5ecae2
Pavel Filippov <pavel_filippov@epam.com>
2021-04-01 17:52:34 +0300

    MSEARCH-30 Implement filtering by updated/created date

--- src/main/java/org/folio/search/utils/SearchQueryUtils.java
+++ src/main/java/org/folio/search/utils/SearchQueryUtils.java
@@ -9,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.index.query.TermQueryBuilder;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -28,10 +29,10 @@ public class SearchQueryUtils {
    * Checks if passed query contains set of disjunction filter conditions.
    *
    * @param query query to analyze
-   * @param filterFieldPredicate predicate to check if subquery is filter query or not.
+   * @param check predicate to check if subquery is filter query or not.
    * @return true if query is disjunction filter query, false - otherwise
    */
-  public static boolean isDisjunctionFilterQuery(QueryBuilder query, Predicate<String> filterFieldPredicate) {
+  public static boolean isDisjunctionFilterQuery(QueryBuilder query, Predicate<String> check) {
     if (!isBoolQuery(query)) {
       return false;
     }
@@ -42,7 +43,7 @@ public class SearchQueryUtils {
 
     String baseFieldName = null;
     for (var innerQuery : boolQuery.should()) {
-      if (!isFilterQuery(innerQuery, filterFieldPredicate)) {
+      if (!isFilterQuery(innerQuery, check)) {
         return false;
       }
       var currentFieldName = ((TermQueryBuilder) innerQuery).fieldName();
@@ -56,7 +57,15 @@ public class SearchQueryUtils {
     return true;
   }
 
-  private static boolean isFilterQuery(QueryBuilder query, Predicate<String> filterFieldPredicate) {
-    return (query instanceof TermQueryBuilder) && filterFieldPredicate.test(((TermQueryBuilder) query).fieldName());
+  /**
+   * Checks if passed query is type of filter or not using given predicate.
+   *
+   * @param query es query as {@link QueryBuilder} object
+   * @param check filter field predicate to test query field
+   * @return true if query is type of filter, false - otherwise
+   */
+  public static boolean isFilterQuery(QueryBuilder query, Predicate<String> check) {
+    return (query instanceof TermQueryBuilder) && check.test(((TermQueryBuilder) query).fieldName())
+      || (query instanceof RangeQueryBuilder) && check.test(((RangeQueryBuilder) query).fieldName());
   }
 }

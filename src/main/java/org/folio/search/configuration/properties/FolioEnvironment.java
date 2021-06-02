a7cb6f98a075b39184b86177b7c8c632d807c06c
Bohdan Suprun <Bohdan_Suprun@epam.com>
2021-06-02 14:55:29 +0300

    MSEARCH-129: Make consumer name different per environment.

--- src/main/java/org/folio/search/configuration/properties/FolioEnvironment.java
+++ src/main/java/org/folio/search/configuration/properties/FolioEnvironment.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class FolioEnvironment {
   private final String okapiUrl;
+  private final String env;
 
-  public FolioEnvironment(@Value("${okapi.url}") String okapiUrl) {
+  public FolioEnvironment(@Value("${okapi.url}") String okapiUrl,
+    @Value("${env:folio}") String env) {
+
     this.okapiUrl = okapiUrl;
+    this.env = env;
   }
 }

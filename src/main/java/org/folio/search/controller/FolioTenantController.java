4235a0b0b8cc1c23c79b03563dceb8100018f3bc
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2021-09-07 10:00:17 +0300

    MSEARCH-174 Investigate indexing issues in reference environment Added support for new tenant attribute to support reindex on module start

--- src/main/java/org/folio/search/controller/FolioTenantController.java
+++ src/main/java/org/folio/search/controller/FolioTenantController.java
@@ -1,8 +1,14 @@
 package org.folio.search.controller;
 
+import static org.folio.search.utils.SearchUtils.INSTANCE_RESOURCE;
+
+import java.util.Collection;
+import java.util.stream.Stream;
 import lombok.extern.log4j.Log4j2;
+import org.folio.search.service.IndexService;
 import org.folio.search.service.KafkaAdminService;
 import org.folio.search.service.SearchTenantService;
+import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.controller.TenantController;
 import org.folio.spring.service.TenantService;
 import org.folio.tenant.domain.dto.TenantAttributes;
@@ -16,15 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("folioTenantController")
 public class FolioTenantController extends TenantController {
 
+  private static final String REINDEX_PARAM_NAME = "runReindex";
+
   private final KafkaAdminService kafkaAdminService;
   private final SearchTenantService tenantService;
+  private final IndexService indexService;
+  private final FolioExecutionContext context;
+
 
   public FolioTenantController(TenantService baseTenantService,
-    KafkaAdminService kafkaAdminService, SearchTenantService tenantService) {
+                               KafkaAdminService kafkaAdminService, SearchTenantService tenantService,
+                               IndexService indexService, FolioExecutionContext context) {
 
     super(baseTenantService);
     this.kafkaAdminService = kafkaAdminService;
     this.tenantService = tenantService;
+    this.indexService = indexService;
+    this.context = context;
   }
 
   @Override
@@ -36,6 +50,14 @@ public class FolioTenantController extends TenantController {
 
     if (tenantInit.getStatusCode() == HttpStatus.OK) {
       tenantService.initializeTenant();
+
+      Stream.ofNullable(tenantAttributes.getParameters()).flatMap(Collection::stream)
+        .filter(parameter -> parameter.getKey().equals(REINDEX_PARAM_NAME)
+          && Boolean.parseBoolean(parameter.getValue()))
+        .findFirst().ifPresent(parameter -> {
+          indexService.dropIndex(INSTANCE_RESOURCE, context.getTenantId());
+          indexService.createIndex(INSTANCE_RESOURCE, context.getTenantId());
+        });
     }
 
     log.info("Tenant init has been completed [response={}]", tenantInit);

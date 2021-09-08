a03ce5b6a3cf048c53e4300f993065acaeb89e20
Oleksii Kuzminov <40833111+OleksiiKuzminov@users.noreply.github.com>
2021-09-08 12:08:20 +0300

    MSEARCH-174 Investigate indexing issues in reference environment (#122)
    * MSEARCH-174 Investigate indexing issues in reference environment
    Added support for new tenant attribute to support reindex on module start

--- src/main/java/org/folio/search/controller/FolioTenantController.java
+++ src/main/java/org/folio/search/controller/FolioTenantController.java
@@ -19,9 +19,8 @@ public class FolioTenantController extends TenantController {
   private final KafkaAdminService kafkaAdminService;
   private final SearchTenantService tenantService;
 
-  public FolioTenantController(TenantService baseTenantService,
-    KafkaAdminService kafkaAdminService, SearchTenantService tenantService) {
-
+  public FolioTenantController(TenantService baseTenantService, KafkaAdminService kafkaAdminService,
+                               SearchTenantService tenantService) {
     super(baseTenantService);
     this.kafkaAdminService = kafkaAdminService;
     this.tenantService = tenantService;
@@ -35,7 +34,7 @@ public class FolioTenantController extends TenantController {
     var tenantInit = super.postTenant(tenantAttributes);
 
     if (tenantInit.getStatusCode() == HttpStatus.OK) {
-      tenantService.initializeTenant();
+      tenantService.initializeTenant(tenantAttributes);
     }
 
     log.info("Tenant init has been completed [response={}]", tenantInit);

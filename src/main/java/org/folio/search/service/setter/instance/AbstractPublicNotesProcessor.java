dd0aebe7d006fd3c849bd3ccc8912229c21b0499
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2021-10-05 14:26:33 +0300

    MSEARCH-175 Item - include circulation notes search in item notes searches

--- src/main/java/org/folio/search/service/setter/instance/AbstractPublicNotesProcessor.java
+++ src/main/java/org/folio/search/service/setter/instance/AbstractPublicNotesProcessor.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
+import org.folio.search.domain.dto.CirculationNote;
 import org.folio.search.domain.dto.Instance;
 import org.folio.search.domain.dto.Note;
 import org.folio.search.service.setter.FieldProcessor;
@@ -14,12 +15,28 @@ public abstract class AbstractPublicNotesProcessor implements FieldProcessor<Ins
 
   @Override
   public Set<String> getFieldValue(Instance instance) {
-    return getNotes(instance)
-      .filter(Objects::nonNull)
-      .filter(note -> note.getStaffOnly() == null || !note.getStaffOnly())
-      .map(Note::getNote)
-      .filter(Objects::nonNull)
-      .collect(toCollection(LinkedHashSet::new));
+    return Stream.concat(
+      getCirculationNotes(instance)
+        .filter(Objects::nonNull)
+        .filter(note -> note.getStaffOnly() == null || !note.getStaffOnly())
+        .map(CirculationNote::getNote)
+        .filter(Objects::nonNull),
+      getNotes(instance).filter(Objects::nonNull)
+        .filter(note -> note.getStaffOnly() == null || !note.getStaffOnly())
+        .map(Note::getNote)
+        .filter(Objects::nonNull)
+    ).collect(toCollection(LinkedHashSet::new));
+
+  }
+
+  /**
+   * Returns {@link Stream} with {@link CirculationNote} objects from holding/item.
+   *
+   * @param instance instance object to analyze
+   * @return {@link Stream} with {@link CirculationNote} object
+   */
+  protected Stream<CirculationNote> getCirculationNotes(Instance instance) {
+    return Stream.empty();
   }
 
   /**

02af96c12b99561fb4273a843529a07305988dc7
Oleksii Kuzminov <40833111+OleksiiKuzminov@users.noreply.github.com>
2021-10-08 13:53:01 +0300

    MSEARCH-175 Item - include circulation notes search in item notes search (#137)
    * MSEARCH-175 Item - include circulation notes search in item notes searches

--- src/main/java/org/folio/search/service/setter/item/ItemPublicNotesProcessor.java
+++ src/main/java/org/folio/search/service/setter/item/ItemPublicNotesProcessor.java
@@ -5,6 +5,7 @@ import static org.folio.search.utils.CollectionUtils.toStreamSafe;
 import java.util.Collection;
 import java.util.stream.Stream;
 import org.apache.commons.collections.CollectionUtils;
+import org.folio.search.domain.dto.CirculationNote;
 import org.folio.search.domain.dto.Instance;
 import org.folio.search.domain.dto.Item;
 import org.folio.search.domain.dto.Note;
@@ -18,6 +19,14 @@ public class ItemPublicNotesProcessor extends AbstractPublicNotesProcessor {
   protected Stream<Note> getNotes(Instance instance) {
     return toStreamSafe(instance.getItems())
       .map(Item::getNotes)
+      .filter(CollectionUtils::isNotEmpty)
+      .flatMap(Collection::stream);
+  }
+
+  @Override
+  protected Stream<CirculationNote> getCirculationNotes(Instance instance) {
+    return toStreamSafe(instance.getItems())
+      .map(Item::getCirculationNotes)
       .filter(CollectionUtils::isNotEmpty)
       .flatMap(Collection::stream);
   }

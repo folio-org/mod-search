2eb4d368b9b6259807263fbdf706740ef9c3f61e
Pavel Filippov <pavel_filippov@epam.com>
2021-11-15 20:19:10 +0300

    MSEARCH-210 Add validation for heading type values

--- src/test/java/org/folio/search/controller/SearchAuthorityIT.java
+++ src/test/java/org/folio/search/controller/SearchAuthorityIT.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.folio.search.sample.SampleAuthorities.getAuthoritySample;
 import static org.folio.search.sample.SampleAuthorities.getAuthoritySampleAsMap;
 import static org.folio.search.sample.SampleAuthorities.getAuthoritySampleId;
-import static org.folio.search.utils.CollectionUtils.toLinkedHashSet;
 import static org.folio.search.utils.TestUtils.parseResponse;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -30,6 +29,8 @@ import org.springframework.beans.BeanUtils;
 
 @IntegrationTest
 class SearchAuthorityIT extends BaseIntegrationTest {
+
+  private static final Set<String> COMMON_FIELDS = Set.of("id", "identifiers", "subjectHeadings", "metadata", "notes");
 
   @BeforeAll
   static void prepare() {
@@ -61,15 +62,15 @@ class SearchAuthorityIT extends BaseIntegrationTest {
   void searchByAuthorities_parameterized_all(String query, String value) throws Exception {
     var response = doSearchByAuthorities(prepareQuery(query, value)).andExpect(jsonPath("$.totalRecords", is(7)));
     var actual = parseResponse(response, AuthoritySearchResult.class);
-    var src = getAuthoritySample();
+    var srcAuthority = getAuthoritySample();
     assertThat(actual.getAuthorities()).isEqualTo(List.of(
-      expectedAuthority(src, "personalName", "sftPersonalName", "saftPersonalName"),
-      expectedAuthority(src, "corporateName", "sftCorporateName", "saftCorporateName"),
-      expectedAuthority(src, "meetingName", "sftMeetingName", "saftMeetingName"),
-      expectedAuthority(src, "geographicName", "sftGeographicTerm", "saftGeographicTerm"),
-      expectedAuthority(src, "uniformTitle", "sftUniformTitle", "saftUniformTitle"),
-      expectedAuthority(src, "topicalTerm", "sftTopicalTerm", "saftTopicalTerm"),
-      expectedAuthority(src, "genreTerm", "sftGenreTerm", "saftGenreTerm")
+      expectedAuthority(srcAuthority, "Personal Name", "personalName", "sftPersonalName", "saftPersonalName"),
+      expectedAuthority(srcAuthority, "Corporate Name", "corporateName", "sftCorporateName", "saftCorporateName"),
+      expectedAuthority(srcAuthority, "Meeting Name", "meetingName", "sftMeetingName", "saftMeetingName"),
+      expectedAuthority(srcAuthority, "Geographic Name", "geographicName", "sftGeographicTerm", "saftGeographicTerm"),
+      expectedAuthority(srcAuthority, "Uniform Title", "uniformTitle", "sftUniformTitle", "saftUniformTitle"),
+      expectedAuthority(srcAuthority, "Topical", "topicalTerm", "sftTopicalTerm", "saftTopicalTerm"),
+      expectedAuthority(srcAuthority, "Genre", "genreTerm", "sftGenreTerm", "saftGenreTerm")
     ));
   }
 
@@ -81,15 +82,17 @@ class SearchAuthorityIT extends BaseIntegrationTest {
     );
   }
 
-  private static Authority expectedAuthority(Authority source, String... fields) {
-    var authority = new Authority();
+  private static Authority expectedAuthority(Authority source, String headingType, String... fields) {
+    var authority = new Authority().headingType(headingType);
+    var fieldsToKeep = Set.of(fields);
 
-    var authorityFields = stream(Authority.class.getDeclaredFields()).map(Field::getName).collect(toLinkedHashSet());
-    authorityFields.removeAll(Set.of("id", "identifiers", "subjectHeadings", "metadata", "notes"));
-    authorityFields.removeAll(Set.of(fields));
+    var authorityFields = stream(Authority.class.getDeclaredFields())
+      .map(Field::getName)
+      .filter(fieldName -> !COMMON_FIELDS.contains(fieldName))
+      .filter(fieldName -> !fieldsToKeep.contains(fieldName))
+      .toArray(String[]::new);
 
-    BeanUtils.copyProperties(source, authority, authorityFields.toArray(String[]::new));
-
+    BeanUtils.copyProperties(source, authority, authorityFields);
     return authority;
   }
 }

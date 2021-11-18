f76152fe9c46d660fe2a6deb848183058230e773
Pavel Filippov <pavel_filippov@epam.com>
2021-11-18 15:57:21 +0300

    MSEARCH-196 Update javadoc for ResourceReindexClient

--- src/main/java/org/folio/search/client/ResourceReindexClient.java
+++ src/main/java/org/folio/search/client/ResourceReindexClient.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @FeignClient("reindex-client")
 public interface ResourceReindexClient {
 
+  /**
+   * Submits reindex request by generated reindex {@link URI} object.
+   *
+   * @param reindexUri - generated reindex uri object
+   * @return response body as {@link ReindexJob} object
+   */
   @PostMapping
   ReindexJob submitReindex(URI reindexUri);
 }

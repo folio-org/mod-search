ec0d1861e43a12494b44441393603c4df24927c6
Pavel Filippov <pavel_filippov@epam.com>
2022-04-04 16:56:17 +0300

    MSEARCH-331 Fix unsupported 'not' operator for search aliases

--- src/main/java/org/folio/search/cql/builders/NotEqualToTermQueryBuilder.java
+++ src/main/java/org/folio/search/cql/builders/NotEqualToTermQueryBuilder.java
@@ -1,18 +1,31 @@
 package org.folio.search.cql.builders;
 
+import static org.elasticsearch.index.query.MultiMatchQueryBuilder.Type.CROSS_FIELDS;
+import static org.elasticsearch.index.query.Operator.AND;
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.multiMatchQuery;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
 import java.util.Set;
 import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NotEqualToTermQueryBuilder implements TermQueryBuilder {
+public class NotEqualToTermQueryBuilder extends FulltextQueryBuilder {
+
+  @Override
+  public QueryBuilder getQuery(Object term, String resource, String... fields) {
+    return boolQuery().mustNot(multiMatchQuery(term, fields).operator(AND).type(CROSS_FIELDS));
+  }
+
+  @Override
+  public QueryBuilder getFulltextQuery(Object term, String fieldName, String resource) {
+    return getQuery(term, resource, updatePathForFulltextQuery(resource, fieldName));
+  }
 
   @Override
   public QueryBuilder getTermLevelQuery(Object term, String fieldName, String resource, String fieldIndex) {
-    return QueryBuilders.boolQuery().mustNot(termQuery(fieldName, term));
+    return boolQuery().mustNot(termQuery(fieldName, term));
   }
 
   @Override

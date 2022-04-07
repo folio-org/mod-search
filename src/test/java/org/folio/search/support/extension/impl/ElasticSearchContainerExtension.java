d23c393be97400b6a773915954a59b90dc90eed1
Pavel Filippov <pavel_filippov@epam.com>
2022-04-07 20:26:31 +0300

    Revert "MSEARCH-302 Return opensearch back"
    This reverts commit 1ea07b8eebb87861f5c4bc05e615993766ccb4c4.

--- src/test/java/org/folio/search/support/extension/impl/ElasticSearchContainerExtension.java
+++ src/test/java/org/folio/search/support/extension/impl/ElasticSearchContainerExtension.java
@@ -10,7 +10,7 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 public class ElasticSearchContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
   private static final String SPRING_PROPERTY_NAME = "spring.elasticsearch.uris";
-  private static final String ES_IMAGE_NAME = "opensearch-it:1.3.1";
+  private static final String ES_IMAGE_NAME = "opendistro-elasticsearch-it:1.13.2";
   private static final Path ES_DOCKERFILE_PATH = Path.of("docker/elasticsearch/Dockerfile");
   private static final GenericContainer<?> CONTAINER = createContainer();
 
@@ -36,7 +36,7 @@ public class ElasticSearchContainerExtension implements BeforeAllCallback, After
     return new GenericContainer<>(new ImageFromDockerfile(ES_IMAGE_NAME, false)
       .withDockerfile(ES_DOCKERFILE_PATH))
       .withEnv("discovery.type", "single-node")
-      .withEnv("plugins.security.disabled", "true")
+      .withEnv("opendistro_security.disabled", "true")
       .withExposedPorts(9200);
   }
 }

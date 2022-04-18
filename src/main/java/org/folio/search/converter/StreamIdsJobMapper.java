a88eed7ad466e5e9b4a2573bbedb17e68c45b234
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2022-04-18 13:37:57 +0200

    MSEARCH-197 POC: Add /authorities/ids API for Authority Records search

--- src/main/java/org/folio/search/converter/StreamIdsJobMapper.java
+++ src/main/java/org/folio/search/converter/StreamIdsJobMapper.java
@@ -1,31 +1,15 @@
 package org.folio.search.converter;
 
-import org.folio.search.domain.dto.FeatureConfig;
 import org.folio.search.domain.dto.StreamIdsJob;
-import org.folio.search.domain.dto.TenantConfiguredFeature;
-import org.folio.search.model.config.FeatureConfigEntity;
 import org.folio.search.model.streamids.StreamIdsJobEntity;
-import org.folio.search.model.streamids.StreamJobStatus;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StreamIdsJobMapper {
 
-  /**
-   * Converts {@link FeatureConfig} object to {@link FeatureConfigEntity} object.
-   *
-   * @param config - dto object to convert as {@link FeatureConfig} object
-   * @return converted {@link FeatureConfigEntity} object
-   */
   StreamIdsJobEntity convert(StreamIdsJob job);
 
-  /**
-   * Converts {@link FeatureConfigEntity} object to {@link FeatureConfig} DTO object.
-   *
-   * @param entity - entity to convert as {@link FeatureConfigEntity} object.
-   * @return converted {@link FeatureConfig} dto object
-   */
   @Mapping(target = "createdDate", dateFormat = "yyyy-MM-dd HH:mm")
   StreamIdsJob convert(StreamIdsJobEntity entity);
 }

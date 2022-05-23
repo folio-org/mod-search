7587d75f8aa8f69c461ca0ad7e046ec648894582
psmagin <Pavlo_Smahin@epam.com>
2022-05-23 18:19:53 +0300

    MSEARCH-352 Create a Contributor Name Type facet

--- src/main/java/org/folio/search/controller/FacetController.java
+++ src/main/java/org/folio/search/controller/FacetController.java
@@ -1,9 +1,11 @@
 package org.folio.search.controller;
 
 import static org.folio.search.utils.SearchUtils.AUTHORITY_RESOURCE;
+import static org.folio.search.utils.SearchUtils.CONTRIBUTOR_RESOURCE;
 import static org.folio.search.utils.SearchUtils.INSTANCE_RESOURCE;
 
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.folio.search.domain.dto.FacetResult;
 import org.folio.search.domain.dto.RecordType;
@@ -21,12 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/search")
 public class FacetController implements RecordTypeApi {
 
+  private static final Map<RecordType, String> recordTypeToResourceMap = Map.of(
+    RecordType.INSTANCES, INSTANCE_RESOURCE,
+    RecordType.AUTHORITIES, AUTHORITY_RESOURCE,
+    RecordType.CONTRIBUTORS, CONTRIBUTOR_RESOURCE
+  );
+
   private final FacetService facetService;
 
   @Override
   public ResponseEntity<FacetResult> getFacets(RecordType recordType, String query,
                                                List<String> facet, String tenantId) {
-    String recordResource = recordType == RecordType.INSTANCES ? INSTANCE_RESOURCE : AUTHORITY_RESOURCE;
+    String recordResource = recordTypeToResourceMap.getOrDefault(recordType, recordType.getValue());
     var facetRequest = CqlFacetRequest.of(recordResource, tenantId, query, facet);
     return ResponseEntity.ok(facetService.getFacets(facetRequest));
   }

e3d3e7ee2858423e31173165118fa91e1d8860e4
Shans Kaluhin <shans_kaluhin@epam.com>
2022-07-07 10:49:01 +0300

    Add @Repository annotation

--- src/main/java/org/folio/search/repository/ResourceIdsJobRepository.java
+++ src/main/java/org/folio/search/repository/ResourceIdsJobRepository.java
@@ -2,7 +2,9 @@ package org.folio.search.repository;
 
 import org.folio.search.model.streamids.ResourceIdsJobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResourceIdsJobRepository extends JpaRepository<ResourceIdsJobEntity, String> {
 
 }

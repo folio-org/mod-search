9362d30ae79c1e60871c6785b6586e93fe7cb710
Sherzod Nurjonov <jd.sherzod@gmail.com>
2022-10-04 16:53:46 +0500

    MSEARCH-435 - added unit test for batch request

--- src/test/java/org/folio/search/integration/ResourceFetchServiceTest.java
+++ src/test/java/org/folio/search/integration/ResourceFetchServiceTest.java
@@ -15,23 +15,31 @@ import static org.folio.search.utils.TestUtils.mapOf;
 import static org.folio.search.utils.TestUtils.randomId;
 import static org.folio.search.utils.TestUtils.resourceEvent;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.folio.search.client.InventoryViewClient;
 import org.folio.search.client.InventoryViewClient.InstanceView;
 import org.folio.search.domain.dto.Holding;
 import org.folio.search.domain.dto.Instance;
 import org.folio.search.domain.dto.Item;
 import org.folio.search.domain.dto.ResourceEvent;
+import org.folio.search.model.service.ResultList;
 import org.folio.search.service.TenantScopedExecutionService;
 import org.folio.search.utils.types.UnitTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 @UnitTest
 @ExtendWith(MockitoExtension.class)
@@ -116,6 +124,38 @@ class ResourceFetchServiceTest {
     assertThat(actual).isEmpty();
   }
 
+  @Test
+  void fetchInstancesByIds_positive_withMoreThan50Resources() {
+    var events = generateResourceEvents(51);
+    var firstPartInstances = events.stream()
+      .limit(50)
+      .map(resourceEvent -> instanceView(new Instance().id(resourceEvent.getId()).title("test"), null))
+      .collect(Collectors.toList());
+    var secondPartInstances = List.of(instanceView(new Instance()
+      .id(events.get(50).getId()).title("inst2").holdings(List.of(new Holding().id("holdingId")))
+      .items(List.of(new Item().id("itemId"))), true));
+
+    when(executionService.executeTenantScoped(any(), any())).thenAnswer(inv -> inv.<Callable<?>>getArgument(1).call());
+    when(inventoryClient.getInstances(any(), anyInt()))
+      .thenAnswer(new Answer<>() {
+        private int count = 0;
+        public ResultList<InstanceView> answer(InvocationOnMock invocation) {
+          if (count++ == 1) {
+            // it will be returned the first time the method is called
+            return asSinglePage(firstPartInstances);
+          }
+          // it will be returned the second time the method is called
+          return asSinglePage(secondPartInstances);
+        }
+      });
+
+    var actual = resourceFetchService.fetchInstancesByIds(events);
+
+    assertThat(actual).hasSize(51);
+    verify(inventoryClient, times(2)).getInstances(any(), anyInt());
+    verify(executionService, times(1)).executeTenantScoped(any(), any());
+  }
+
   private static List<ResourceEvent> resourceEvents() {
     var updateEventId = randomId();
     return List.of(
@@ -131,5 +171,11 @@ class ResourceFetchServiceTest {
     var itemsMap = OBJECT_MAPPER.convertValue(instance.getItems(), LIST_OF_MAPS_TYPE_REFERENCE);
     var holdingsMap = OBJECT_MAPPER.convertValue(instance.getHoldings(), LIST_OF_MAPS_TYPE_REFERENCE);
     return new InstanceView(instanceMap, holdingsMap, itemsMap, isBoundWith);
+  }
+
+  private static List<ResourceEvent> generateResourceEvents(int number) {
+    return Stream.generate(() -> resourceEvent(randomId(), INSTANCE_RESOURCE, CREATE))
+      .limit(number)
+      .collect(Collectors.toList());
   }
 }

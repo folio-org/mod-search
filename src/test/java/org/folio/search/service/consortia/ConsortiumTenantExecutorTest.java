999cc6ed8ce6d255316c8418b8e2b82955eb11f7
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2023-09-15 15:49:38 +0200

    - Fix consortia tests

--- src/test/java/org/folio/search/service/consortia/ConsortiumTenantExecutorTest.java
+++ src/test/java/org/folio/search/service/consortia/ConsortiumTenantExecutorTest.java
@@ -98,11 +98,19 @@ class ConsortiumTenantExecutorTest {
   }
 
   private Supplier<String> operation() {
-    return () -> OPERATION_RESPONSE_MOCK;
+    return new Supplier<String>() {
+      @Override
+      public String get() {
+        return OPERATION_RESPONSE_MOCK;
+      }
+    };
   }
 
   private Runnable operationRunnable() {
-    return () -> {
+    return new Runnable() {
+      @Override
+      public void run() {
+      }
     };
   }
 

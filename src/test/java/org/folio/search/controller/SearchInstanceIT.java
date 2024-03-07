0c98bd418f2581ac353ebac129285dfd3d708ee2
Viacheslav_Poliakov <Viacheslav_Poliakov@epam.com>
2024-03-07 17:39:57 +0200

    feat(mod-search): add ability to case insensitive search
    Add ability to case insensitive search ISSNs with trailing roman numerals
    
    - Add case insensitive type to instance model
    - Add test case in SearchInstanceIT to handle this ability
    
    Closes: MSEARCH-672

--- src/test/java/org/folio/search/controller/SearchInstanceIT.java
+++ src/test/java/org/folio/search/controller/SearchInstanceIT.java
@@ -66,6 +66,15 @@ class SearchInstanceIT extends BaseIntegrationTest {
       .andExpect(jsonPath("$.instances[0].id", is(getSemanticWebId())));
   }
 
+  @MethodSource("testIssnDataProvider")
+  @DisplayName("search by instances case insensitive ISSN with trailing X")
+  @ParameterizedTest(name = "[{index}] query={0}, value=''{1}''")
+  void searchByInstancesCaseInsensitiveIssn_parameterized_singleResult(String query, String value) throws Throwable {
+    doSearchByInstances(prepareQuery(query, value))
+      .andExpect(jsonPath("$.totalRecords", is(1)))
+      .andExpect(jsonPath("$.instances[0].id", is(getSemanticWebId())));
+  }
+
   @ParameterizedTest(name = "[{index}] {0}")
   @CsvSource({
     "title == {value}, web semantic",
@@ -419,5 +428,15 @@ class SearchInstanceIT extends BaseIntegrationTest {
       arguments("keyword ==/string {value}", "0262012103"),
       arguments("(title all \"{value}\")", "A semantic web primer : 0747-0850")
     );
+  }
+
+  private static Stream<Arguments> testIssnDataProvider() {
+    return Stream.of(
+      arguments("issn = {value}", "0040-781X"),
+      arguments("issn = {value}", "0040-781x"),
+      arguments("issn = {value}", "*0-781X"),
+      arguments("issn = {value}", "*0-781x"),
+      arguments("issn = {value}", "0040-781*")
+      );
   }
 }

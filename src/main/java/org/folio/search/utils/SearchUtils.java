0471b33947048484709b59c6afe0d44794f26f30
Aleksei Pronichev <aleksei_pronichev@epam.com>
2024-05-23 17:14:48 +0500

    MSEARCH-781: Add bibframe search functionality

--- src/main/java/org/folio/search/utils/SearchUtils.java
+++ src/main/java/org/folio/search/utils/SearchUtils.java
@@ -38,6 +38,7 @@ public class SearchUtils {
   public static final String CONTRIBUTOR_RESOURCE = getResourceName(Contributor.class);
   public static final String LOCATION_RESOURCE = "location";
   public static final String CLASSIFICATION_TYPE_RESOURCE = "classification-type";
+  public static final String BIBFRAME_RESOURCE = "bibframe";
 
   public static final String ID_FIELD = "id";
   public static final String SOURCE_FIELD = "source";

dd8fe470643b28083c3fdd67c745f862139090d5
psmagin <pavlo_smahin@epam.com>
2024-05-27 15:29:53 +0300

    - disable sub indexes

--- src/main/java/org/folio/search/service/ResourceService.java
+++ src/main/java/org/folio/search/service/ResourceService.java
@@ -155,11 +155,8 @@ public class ResourceService {
   private Map<String, List<SearchDocumentBody>> processIndexInstanceEvents(List<ResourceEvent> resourceEvents) {
     var indexEvents = extractEventsForDataMove(resourceEvents);
     var fetchedInstances = resourceFetchService.fetchInstancesByIds(indexEvents);
-    messageProducer.prepareAndSendContributorEvents(fetchedInstances);
-    messageProducer.prepareAndSendSubjectEvents(fetchedInstances);
 
-    var list = preProcessEvents(fetchedInstances, consortiumInstanceService::saveInstances);
-    return multiTenantSearchDocumentConverter.convert(list);
+    return multiTenantSearchDocumentConverter.convert(fetchedInstances);
   }
 
   private List<ResourceEvent> preProcessEvents(List<ResourceEvent> instanceEvents,

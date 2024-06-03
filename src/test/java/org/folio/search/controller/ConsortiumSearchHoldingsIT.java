96c5776197c0715cdca75b98d8ca4a62dbecca55
Viacheslav Kolesnyk <94473337+viacheslavkol@users.noreply.github.com>
2024-06-03 16:02:58 +0200

    fix(consortium-search): Limit number of ids passed to consortium batch items/holdings apis (#595)
    * fix(consortium-search): Limit number of ids passed to consortium batch items/holdings apis
    
    Refs: MSEARCH-759

--- src/test/java/org/folio/search/controller/ConsortiumSearchHoldingsIT.java
+++ src/test/java/org/folio/search/controller/ConsortiumSearchHoldingsIT.java
@@ -1,6 +1,7 @@
 package org.folio.search.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.folio.search.controller.ConsortiumSearchItemsIT.WRONG_SIZE_MSG;
 import static org.folio.search.controller.SearchConsortiumController.REQUEST_NOT_ALLOWED_MSG;
 import static org.folio.search.model.Pair.pair;
 import static org.folio.search.sample.SampleInstances.getSemanticWeb;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 import org.folio.search.domain.dto.BatchIdsDto;
 import org.folio.search.domain.dto.ConsortiumHolding;
 import org.folio.search.domain.dto.ConsortiumHoldingCollection;
@@ -139,6 +141,23 @@ class ConsortiumSearchHoldingsIT extends BaseConsortiumIntegrationTest {
       .andExpect(jsonPath("$.errors[0].code", is("validation_error")))
       .andExpect(jsonPath("$.errors[0].parameters[0].key", is("x-okapi-tenant")))
       .andExpect(jsonPath("$.errors[0].parameters[0].value", is(MEMBER_TENANT_ID)));
+  }
+
+  @Test
+  void tryGetConsortiumBatchHoldings_returns400_whenMoreIdsThanLimit() throws Exception {
+    var request = new BatchIdsDto()
+      .ids(
+        Stream.iterate(0, i -> i < 1001, i -> ++i)
+          .map(i -> UUID.randomUUID())
+          .toList()
+      );
+
+    tryPost(consortiumBatchHoldingsSearchPath(), request)
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.errors[0].message", is(WRONG_SIZE_MSG)))
+      .andExpect(jsonPath("$.errors[0].type", is("MethodArgumentNotValidException")))
+      .andExpect(jsonPath("$.errors[0].code", is("validation_error")))
+      .andExpect(jsonPath("$.errors[0].parameters[0].key", is("ids")));
   }
 
   private ConsortiumHolding[] getExpectedHoldings() {

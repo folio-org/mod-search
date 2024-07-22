f28dfebae7e9ed922e44a146b41fb0189b4e6405
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2024-07-22 12:29:55 +0500

    MSEARCH-793: apply comments

--- src/main/resources/changelog/changes/v4.0/create-instance-trigger.sql
+++ src/main/resources/changelog/changes/v4.0/create-instance-trigger.sql
@@ -6,61 +6,65 @@ BEGIN
   -- extract classifications
   FOR i IN SELECT * FROM jsonb_array_elements(NEW.instance_json -> 'classifications') LOOP
     INSERT
-    INTO classifications(classification_number, classification_type_id, instances_arr)
-    VALUES (i ->> 'classificationNumber',
+    INTO classification(id, number, typeId, instances)
+    VALUES (encode(digest((i ->> 'classificationNumber' || i ->> 'classificationTypeId')::bytea, 'sha1'), 'hex'),
+            i ->> 'classificationNumber',
             i ->> 'classificationTypeId',
             jsonb_build_array(
               json_build_object(
-                'instanceId', NEW.instance_id,
+                'instanceId', NEW.id,
                 'shared', NEW.shared,
                 'tenantId', NEW.tenant_id
               )
             ))
-    ON CONFLICT (classification_type_id, classification_number)
-      DO UPDATE SET instances_arr = classifications.instances_arr || excluded.instances_arr;
+    ON CONFLICT (id)
+      DO UPDATE SET instances = classification.instances || excluded.instances;
   END LOOP;
 
   -- extract subjects
   FOR i IN SELECT * FROM jsonb_array_elements(NEW.instance_json -> 'subjects') LOOP
     INSERT
-    INTO subjects(subject_id, subject_value, authority_id, instances_arr)
+    INTO subject(id, value, authorityId, instances)
     VALUES (encode(digest((i ->> 'value' || i ->> 'authorityId')::bytea, 'sha1'), 'hex'), -- requires pgcrypto enabled
             i ->> 'value',
+            i ->> 'authorityId',
             jsonb_build_array(
               json_build_object(
-                'instanceId', NEW.instance_id,
+                'instanceId', NEW.id,
                 'shared', NEW.shared,
                 'tenantId', NEW.tenant_id
               )
             ))
-    ON CONFLICT (subject_id, subject_value)
-      DO UPDATE SET instances_arr = subjects.instances_arr || excluded.instances_arr;
+    ON CONFLICT (id)
+      DO UPDATE SET instances = subject.instances || excluded.instances;
   END LOOP;
 
   -- extract contributors
   FOR i IN SELECT * FROM jsonb_array_elements(NEW.instance_json -> 'contributors') LOOP
     INSERT
-    INTO contributors(contributor_id, contributor_name, instances_arr)
+    INTO contributor(id, name, contributorNameTypeId, authorityId, instances)
     VALUES (encode(digest((i ->> 'name' || i ->> 'authorityId')::bytea, 'sha1'), 'hex'), -- requires pgcrypto enabled
             i ->> 'name' ,
+            i ->> 'contributorNameTypeId',
+            i ->> 'authorityId',
             jsonb_build_array(
               json_build_object(
-                'instanceId', NEW.instance_id,
+                'instanceId', NEW.id,
                 'shared', NEW.shared,
                 'tenantId', NEW.tenant_id,
-                'contributorTypeId', i ->> 'contributorNameTypeId'
+                'contributorTypeId', i ->> 'contributorTypeId'
               )
             ))
-    ON CONFLICT (contributor_id, contributor_name)
-      DO UPDATE SET instances_arr = contributors.instances_arr || excluded.instances_arr;
+    ON CONFLICT (id)
+      DO UPDATE SET instances = contributor.instances || excluded.instances;
   END LOOP;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS instance_trigger ON instances CASCADE;
+DROP TRIGGER IF EXISTS instance_trigger ON instance CASCADE;
 CREATE TRIGGER instance_trigger
   AFTER INSERT OR UPDATE
-  ON instances
+  ON instance
   FOR EACH ROW
   EXECUTE FUNCTION instance_deps_trigger();

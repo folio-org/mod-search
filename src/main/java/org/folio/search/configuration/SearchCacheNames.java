d1ad4e451df4c4fe6c9994d74d4ef3bfcb101c63
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2024-12-10 18:42:29 +0100

    fix(browse): Remove browse config caching
    - Remove the caching to fix issue with old browse config being used
    
    Closes: MSEARCH-897

--- src/main/java/org/folio/search/configuration/SearchCacheNames.java
+++ src/main/java/org/folio/search/configuration/SearchCacheNames.java
@@ -8,7 +8,6 @@ public class SearchCacheNames {
 
   public static final String ES_INDICES_CACHE = "es-indices";
   public static final String REFERENCE_DATA_CACHE = "reference-data-cache";
-  public static final String BROWSE_CONFIG_CACHE = "browse-config-cache";
   public static final String RESOURCE_LANGUAGE_CACHE = "tenant-languages";
   public static final String TENANT_FEATURES_CACHE = "tenant-features";
   public static final String SEARCH_PREFERENCE_CACHE = "search-preference";

01f598ca277a932c44ad15b83b697b5f2ce8176e
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2025-07-29 19:07:44 +0300

    feat(remove-unused-fields): add configuration for objectMapper

--- src/main/java/org/folio/search/utils/JsonConverter.java
+++ src/main/java/org/folio/search/utils/JsonConverter.java
@@ -2,8 +2,10 @@ package org.folio.search.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -29,6 +31,12 @@ public class JsonConverter {
   public static final String DESERIALIZATION_ERROR_MSG_TEMPLATE = "Failed to deserialize value [value: {}]";
 
   private final ObjectMapper objectMapper;
+
+  @PostConstruct
+  public void init() {
+    objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
+    objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+  }
 
   /**
    * Converts {@link String} value as {@link T} class value.

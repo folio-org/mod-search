052d9fe91a94b4201af697048c1c964e01586e20
Pavlo Smahin <Pavlo_Smahin@epam.com>
2025-11-06 17:25:23 +0200

    feat: add retry to reindexing logic on enabling the tenant (#860)
    * feat: add retry to reindexing logic on enabling the tenant
    
    Implements: MSEARCH-1111

--- src/main/java/org/folio/search/SearchApplication.java
+++ src/main/java/org/folio/search/SearchApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Folio search application.
  */
+@EnableRetry
 @EnableCaching
 @EnableScheduling
 @EnableFeignClients

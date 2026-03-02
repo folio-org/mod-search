336884b1ed5f10c7ead774abdd57fc05342ec2e0
psmagin <pavlo_smahin@epam.com>
2026-03-02 15:18:07 +0200

    add remote storage config validation

--- src/main/java/org/folio/search/configuration/properties/RemoteStorageProperties.java
+++ src/main/java/org/folio/search/configuration/properties/RemoteStorageProperties.java
@@ -1,18 +1,26 @@
 package org.folio.search.configuration.properties;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 @Configuration
 @ConfigurationProperties("folio.remote-storage")
+@ConditionalOnProperty(name = "folio.reindex.reindex-type", havingValue = "EXPORT")
 public class RemoteStorageProperties {
 
+  @NotNull(message = "Remote storage endpoint is required")
   private String endpoint;
+  @NotNull(message = "Remote storage region is required")
   private String region;
+  @NotNull(message = "Remote storage bucket is required")
   private String bucket;
   private String accessKey;
   private String secretKey;
-  private boolean awsSdk;
+  private boolean awsSdk = true;
 }
